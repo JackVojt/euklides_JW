a372b5411bf7a9a56cf00d078d98569083841584
Jacek.Woj <jacek.wojtuszek@gmail.com>
2019-05-12 12:36:45 +0100

    euklides_rozwiazany

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,13 +1,16 @@
 package com.sda.algorytmy;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        int a = 12;
+        int b = 18;
+        while (a != b){
+            if (a > b){
+                a = a - b;
+                }else{
+                b = b - a;
+            }
+        }
+        System.out.println(a);
     }
 }
